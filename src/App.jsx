1ef2bda742372f8a89f3029294680f92a722169d
Thomas Erdmenger <thomas.erdmenger@icloud.com>
2023-10-27 11:10:27 +0200

    Add State Hook for counter

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,23 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faRotate } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
+  const [counter, setCounter] = React.useState(0);
+
+  const handleMinusClick = () => setCounter((prevCounter) => prevCounter - 1);
+  const handlePlusClick = () => setCounter((prevCounter) => prevCounter + 1);
+
   return (
     <div className="counter-container">
       <h1 className="counter-heading">Fancy Counter</h1>
-      <span className="counter">0</span>
+      <span className="counter">{counter}</span>
       <FontAwesomeIcon icon={faRotate} />
       <div className="counter-buttons-container">
-        <button className="counter-buttons">-</button>
-        <button className="counter-buttons">+</button>
+        <button className="counter-buttons" onClick={handleMinusClick}>
+          -
+        </button>
+        <button className="counter-buttons" onClick={handlePlusClick}>
+          +
+        </button>
       </div>
     </div>
   );
