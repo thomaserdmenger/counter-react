18a94285ee788834b966c29d5cf516296b0a00d4
Thomas Erdmenger <thomas.erdmenger@icloud.com>
2023-10-27 11:07:43 +0200

    Add elements and basic styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,22 @@
-function App() {}
+import * as React from "react";
+
+import "./styles.css";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faRotate } from "@fortawesome/free-solid-svg-icons";
+
+function App() {
+  return (
+    <div className="counter-container">
+      <h1 className="counter-heading">Fancy Counter</h1>
+      <span className="counter">0</span>
+      <FontAwesomeIcon icon={faRotate} />
+      <div className="counter-buttons-container">
+        <button className="counter-buttons">-</button>
+        <button className="counter-buttons">+</button>
+      </div>
+    </div>
+  );
+}
 
 export default App;
